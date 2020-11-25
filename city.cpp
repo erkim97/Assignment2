506b3517d615f78f47596c173666d875b196dd29
Sehyung (Eric) Kim <kimeric97@hotmail.com>
2020-11-24 19:46:59 -0800

    City class created

---
+++ city.cpp
@@ -0,0 +1,16 @@
+//
+// Created by kimer on 2020-11-24.
+//
+
+#include "city.hpp"
+
+/*
+ * city constructor
+ * @params char name : name of the city
+ *         int x : x coordinate of the city
+ *         int y : y coordinate of the city
+ * @returns city : newly created city
+ */
+city::city(char name, int x, int y): name(name), x(x), y(y) {
+
+}
---
+++ city.hpp
@@ -0,0 +1,19 @@
+//
+// Created by kimer on 2020-11-24.
+//
+
+#ifndef ASSIGNMENT2_CITY_HPP
+#define ASSIGNMENT2_CITY_HPP
+
+
+class city {
+public:
+    char name;
+    int x;
+    int y;
+
+    city(char name, int x, int y);
+};
+
+
+#endif //ASSIGNMENT2_CITY_HPP
